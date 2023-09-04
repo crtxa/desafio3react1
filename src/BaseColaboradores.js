c48656c0b5ae06ceb9a3cdd29305de8a4c60a9e0
crtorres <crtx.1991@gmail.com>
2023-09-04 00:55:19 -0300

    initial commit

---
+++ src/BaseColaboradores.js
@@ -0,0 +1,26 @@
+export const BaseColaboradores = [
+    {
+      id: '1',
+      nombre: 'Pedro Pretzel',
+      correo: 'pedrito@colaborador.com',
+      edad: '32',
+      cargo: 'Dev FrontEnd',
+      telefono: '99999999',
+    },
+    {
+      id: '2',
+      nombre: 'Juan Riquelme',
+      correo: 'juanito@colaborador.com',
+      edad: '31',
+      cargo: 'Dev Backend',
+      telefono: '98989898',
+    },
+    {
+      id: '3',
+      nombre: 'Antonio Sepulveda',
+      correo: 'antonito@colaborador.com',
+      edad: '39',
+      cargo: 'DevOps',
+      telefono: '99889988',
+    },
+  ];
